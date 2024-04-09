1370563c9677992ec2e9357f70ca805a2dc2a1f7
Wenxin Wang <wenxin.wang.engineer@gmail.com>
2024-04-09 15:57:12 -0700

    Pop up a window to auto play generated video

--- Main.py
+++ Main.py
@@ -12,7 +12,6 @@ This script will:
 @Date: April 4, 2024
 '''
 import os
-import cv2
 
 import FetchGnews
 import FetchImage
@@ -71,17 +70,7 @@ def main(topics):
                                                  video_file)
 
                     # pop up the video
-                    cap = cv2.VideoCapture(video_file)
-                    while (cap.isOpened()):
-                        ret, frame = cap.read()
-                        if ret:
-                            cv2.imshow('Video', frame)
-                            if cv2.waitKey(1) & 0xFF == ord('q'):
-                                break
-                        else:
-                            break
-                    cap.release()
-                    cv2.destroyAllWindows()
+                    os.system(f"open {video_file}")
 
                 else:
                     # TBD: setup default voice over captain without photo
